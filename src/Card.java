bb7967cd684d6801bee8889109213b4039ff0108
HKing2802 <croppa1@yahoo.com>
2021-06-29 21:05:00 -0400

    Added javadocs

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,20 @@
+/**
+ * Class to represent a singular Sabacc card
+ *
+ * @author HKing
+ */
 public class Card {
+    // the numerical value of the card
     private final int value;
+
+    // the stave (or suit) of the card
     private final Stave stave;
 
+    /**
+     * Initializes a Card with a given value and stave
+     * @param value The numerical value of the card
+     * @param stave The stave of the card
+     */
     public Card(int value, Stave stave) {
         this.value = value;
         this.stave = stave;
@@ -15,5 +28,9 @@ public class Card {
         return this.stave;
     }
 
-    //TODO: toString
+    @Override
+    public String toString() {
+
+        return null;
+    }
 }
